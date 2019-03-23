177f8f7188073ba5476a963d090f7bb86ed66fc8
jaypsaade <jaypsaade@gmail.com>
2019-03-23 17:19:39 +0200

    replaced '?' with degree code

--- tp2/question2/FahrenheitCelsiusTest.java
+++ tp2/question2/FahrenheitCelsiusTest.java
@@ -1,11 +1,11 @@
 package question2;
 public class FahrenheitCelsiusTest extends junit.framework.TestCase
 {
-    // Définissez ici les variables d'instance nécessaires à vos engagements;
-    // Vous pouvez également les saisir automatiquement du présentoir
-    // à l'aide du menu contextuel "Présentoir --> Engagements".
+    // Dï¿½finissez ici les variables d'instance nï¿½cessaires ï¿½ vos engagements;
+    // Vous pouvez ï¿½galement les saisir automatiquement du prï¿½sentoir
+    // ï¿½ l'aide du menu contextuel "Prï¿½sentoir --> Engagements".
     // Notez cependant que ce dernier ne peut saisir les objets primitifs
-    // du présentoir (les objets sans constructeur, comme int, float, etc.).
+    // du prï¿½sentoir (les objets sans constructeur, comme int, float, etc.).
 
     /**
      * Constructeur de la classe-test FahrenheitCelsiusTest
@@ -17,7 +17,7 @@ public class FahrenheitCelsiusTest extends junit.framework.TestCase
     /**
      * Met en place les engagements.
      *
-     * Méthode appelée avant chaque appel de méthode de test.
+     * Mï¿½thode appelï¿½e avant chaque appel de mï¿½thode de test.
      */
     protected void setUp() // throws java.lang.Exception
     {
@@ -28,18 +28,19 @@ public class FahrenheitCelsiusTest extends junit.framework.TestCase
     /**
      * Supprime les engagements
      *
-     * Méthode appelée après chaque appel de méthode de test.
+     * Mï¿½thode appelï¿½e aprï¿½s chaque appel de mï¿½thode de test.
      */
     protected void tearDown() // throws java.lang.Exception
     {
-        //Libérez ici les ressources engagées par setUp()
+        //Libï¿½rez ici les ressources engagï¿½es par setUp()
     }
 
    public void test_fahrenheitEnCelsius() {
-		assertEquals("    0 °F -> -17.7 °C ? ", -17.7, question2.FahrenheitCelsius.fahrenheitEnCelsius(0), 0.1);
-		assertEquals("  100 °F -> 37.7 °C ? ", 37.7, question2.FahrenheitCelsius.fahrenheitEnCelsius(100), 0.1);
-		assertEquals(" 2000 °F -> 1093.3 °C ?", 1093.3, question2.FahrenheitCelsius.fahrenheitEnCelsius(2000), 0.1);
-		assertEquals("   54 °F -> 12.2 °C ?", 12.2, question2.FahrenheitCelsius.fahrenheitEnCelsius(54), 0.1);
+        final String degre = "\u00B0";
+        assertEquals("    0 " + degre + "F -> -17.7 " + degre + "C ? ", -17.7, question1.FahrenheitCelsius.fahrenheitEnCelsius(0), 0.1);
+        assertEquals("  100 " + degre + "F -> 37.7 " + degre + "C ? ", 37.7, question1.FahrenheitCelsius.fahrenheitEnCelsius(100), 0.1);
+        assertEquals(" 2000 " + degre + "F -> 1093.3 " + degre + "C ?", 1093.3, question1.FahrenheitCelsius.fahrenheitEnCelsius(2000), 0.1);
+        assertEquals("   54 " + degre + "F -> 12.2 " + degre + "C ?", 12.2, question1.FahrenheitCelsius.fahrenheitEnCelsius(54), 0.1);
 	}
 
 }
